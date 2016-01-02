b7bb68d462b856f6c673a230ee47dd52b17e703f
butchcanada <butch.canada1617@gmail.com>
2016-01-03 06:05:23 +0800

    Videoke Rental Commit: Untrack Files

---
+++ smartshop/Controllers/SoftwareController.cs
@@ -0,0 +1,43 @@
+﻿using VideokeRental.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideokeRental.Controllers
+{
+    public class SoftwareController : UserAccountController {
+        
+        [Authorize]
+        // GET: Software
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        // GET: Software
+        public ActionResult Product()
+        {
+            return View();
+        }
+        [Authorize]
+        // GET: Software
+        public ActionResult Customer()
+        {
+            return View();
+        }
+        [Authorize]
+        // GET: Software
+        public ActionResult Users()
+        {
+            return View();
+        }
+        [Authorize]
+        // GET: Software
+        public ActionResult Message()
+        {
+            return View();
+        }
+    }
+}
