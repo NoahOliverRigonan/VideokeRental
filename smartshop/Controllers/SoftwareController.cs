48f7cf010d40249e32011792ee7c60f8b32000a4
butchcanada <butch.canada1617@gmail.com>
2016-02-17 03:34:57 +0800

    B-JAM Print Calendar reports

--- smartshop/Controllers/SoftwareController.cs
+++ smartshop/Controllers/SoftwareController.cs
@@ -10,8 +10,11 @@ using iTextSharp.text.pdf;
 
 namespace VideokeRental.Controllers
 {
-    public class SoftwareController : UserAccountController {
-        
+    public class SoftwareController : UserAccountController
+    {
+
+        private Data.videokerentalDataContext db = new Data.videokerentalDataContext();
+
         [Authorize]
         // GET: Software
         public ActionResult Index()
@@ -62,8 +65,20 @@ namespace VideokeRental.Controllers
         }
         [Authorize]
         // GET: Software
-        public ActionResult CalendarReservePDF(String date)
+        public ActionResult CalendarReservePDF(String DateFrom, String DateTo)
         {
+            var calendarResevations = from d in db.tblCalendarReservations
+                                      where d.ReservedDate >= Convert.ToDateTime(DateFrom) && d.ReservedDate <= Convert.ToDateTime(DateTo)
+                                      select new Models.tblCalendarReservation
+                                      {
+                                          Id = d.Id,
+                                          CustomersIdReserved = d.CustomersIdReserved,
+                                          ProductId = d.ProductId,
+                                          Product = d.tblProduct.ProductName,
+                                          CustomersReserved = d.tblCustomer.Customer,
+                                          ReservedDate = d.ReservedDate.ToShortDateString(),
+                                      };
+
             // Start of the PDF
             MemoryStream workStream = new MemoryStream();
             Rectangle rec = new Rectangle(PageSize.A3);
@@ -73,8 +88,62 @@ namespace VideokeRental.Controllers
             // Document Starts
             document.Open();
 
+            // Fonts Customization
+            Font headerFont = FontFactory.GetFont("Arial", 17, Font.BOLD);
+            Font headerDetailFont = FontFactory.GetFont("Arial", 11);
+            Font columnFont = FontFactory.GetFont("Arial", 11, Font.BOLD);
+            Font cellFont = FontFactory.GetFont("Arial", 11);
+
+            // table main header
+            PdfPTable tableHeader = new PdfPTable(2);
+            float[] widthscellsheader = new float[] { 100f, 75f };
+            tableHeader.SetWidths(widthscellsheader);
+            tableHeader.WidthPercentage = 100;
+            tableHeader.AddCell(new PdfPCell(new Phrase("Calendar Reservation List - Report", headerFont)) { Border = 0, HorizontalAlignment = 0 });
+            tableHeader.AddCell(new PdfPCell(new Phrase("Printed " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToString("hh:mm:ss tt"), headerDetailFont))
+            {
+                Border = 0,
+                HorizontalAlignment = 2,
+                PaddingTop = 5f
+            });
+            tableHeader.AddCell(new PdfPCell(new Phrase("Date from " + DateFrom + " to " + DateTo, cellFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+            tableHeader.AddCell(new PdfPCell(new Phrase(" ", cellFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+            document.Add(tableHeader);
+
             Paragraph line = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 1)));
             document.Add(line);
+
+            PdfPTable tableForCalendarReservation = new PdfPTable(3);
+            float[] widthscellsheaderForCalendarReservation = new float[] { 50f, 100f, 70f };
+            tableForCalendarReservation.SetWidths(widthscellsheaderForCalendarReservation);
+            tableForCalendarReservation.WidthPercentage = 100;
+            tableForCalendarReservation.AddCell(new PdfPCell(new Phrase("Reserve Date", columnFont)) { HorizontalAlignment = 1, PaddingBottom = 5f, BackgroundColor = BaseColor.LIGHT_GRAY });
+            tableForCalendarReservation.AddCell(new PdfPCell(new Phrase("Videoke", columnFont)) { HorizontalAlignment = 1, PaddingBottom = 5f, BackgroundColor = BaseColor.LIGHT_GRAY });
+            tableForCalendarReservation.AddCell(new PdfPCell(new Phrase("Customer", columnFont)) { HorizontalAlignment = 1, PaddingBottom = 5f, BackgroundColor = BaseColor.LIGHT_GRAY });
+
+            foreach (var calendarReserve in calendarResevations)
+            {
+                tableForCalendarReservation.AddCell(new PdfPCell(new Phrase(calendarReserve.ReservedDate.ToString(), cellFont)) { PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                tableForCalendarReservation.AddCell(new PdfPCell(new Phrase(calendarReserve.Product, cellFont)) { PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                tableForCalendarReservation.AddCell(new PdfPCell(new Phrase(calendarReserve.CustomersReserved, cellFont)) { PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+            }
+            document.Add(tableForCalendarReservation);
+
+            document.Add(Chunk.NEWLINE);
+
+            var NumberOfReservation = calendarResevations.Count();
+
+            PdfPTable tableForCalendarReservationFooter = new PdfPTable(3);
+            float[] widthscellsheaderForCalendarReservationFooter = new float[] { 50f, 100f, 70f };
+            tableForCalendarReservationFooter.SetWidths(widthscellsheaderForCalendarReservationFooter);
+            tableForCalendarReservationFooter.WidthPercentage = 100;
+            tableForCalendarReservationFooter.AddCell(new PdfPCell(new Phrase("", columnFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+            tableForCalendarReservationFooter.AddCell(new PdfPCell(new Phrase("No of Reservations:", columnFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f, HorizontalAlignment = 2 });
+            tableForCalendarReservationFooter.AddCell(new PdfPCell(new Phrase(NumberOfReservation.ToString(), columnFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+
+            document.Add(tableForCalendarReservationFooter);
 
             // Document End
             document.Close();
@@ -88,8 +157,20 @@ namespace VideokeRental.Controllers
 
         [Authorize]
         // GET: Software
-        public ActionResult CalendarRentalPDF(String date)
+        public ActionResult CalendarRentalPDF(String DateFrom, String DateTo)
         {
+            var calendarRentals = from d in db.tblCalendarRentals
+                                  where d.RentedDate >= Convert.ToDateTime(DateFrom) && d.RentedDate <= Convert.ToDateTime(DateTo)
+                                  select new Models.tblCalendarRental
+                                  {
+                                      Id = d.Id,
+                                      CustomersIdRented = d.CustomersIdRented,
+                                      CustomersRent = d.tblCustomer.Customer,
+                                      ProductId = d.ProductId,
+                                      Product = d.tblProduct.ProductName,
+                                      RentedDate = d.RentedDate.ToShortDateString(),
+                                  };
+
             // Start of the PDF
             MemoryStream workStream = new MemoryStream();
             Rectangle rec = new Rectangle(PageSize.A3);
@@ -99,8 +180,62 @@ namespace VideokeRental.Controllers
             // Document Starts
             document.Open();
 
+            // Fonts Customization
+            Font headerFont = FontFactory.GetFont("Arial", 17, Font.BOLD);
+            Font headerDetailFont = FontFactory.GetFont("Arial", 11);
+            Font columnFont = FontFactory.GetFont("Arial", 11, Font.BOLD);
+            Font cellFont = FontFactory.GetFont("Arial", 11);
+
+            // table main header
+            PdfPTable tableHeader = new PdfPTable(2);
+            float[] widthscellsheader = new float[] { 100f, 75f };
+            tableHeader.SetWidths(widthscellsheader);
+            tableHeader.WidthPercentage = 100;
+            tableHeader.AddCell(new PdfPCell(new Phrase("Calendar Rental List - Report", headerFont)) { PaddingBottom = 5f, Border = 0, HorizontalAlignment = 0 });
+            tableHeader.AddCell(new PdfPCell(new Phrase("Printed " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToString("hh:mm:ss tt"), headerDetailFont))
+            {
+                Border = 0,
+                HorizontalAlignment = 2,
+                PaddingTop = 5f
+            });
+            tableHeader.AddCell(new PdfPCell(new Phrase("Date from " + DateFrom + " to " + DateTo, cellFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+            tableHeader.AddCell(new PdfPCell(new Phrase(" ", cellFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+            document.Add(tableHeader);
+
             Paragraph line = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 1)));
             document.Add(line);
+
+            PdfPTable tableForCalendarRental = new PdfPTable(3);
+            float[] widthscellsheaderForCalendarRental = new float[] { 50f, 100f, 70f };
+            tableForCalendarRental.SetWidths(widthscellsheaderForCalendarRental);
+            tableForCalendarRental.WidthPercentage = 100;
+            tableForCalendarRental.AddCell(new PdfPCell(new Phrase("Rental Date", columnFont)) { HorizontalAlignment = 1, PaddingBottom = 5f, BackgroundColor = BaseColor.LIGHT_GRAY });
+            tableForCalendarRental.AddCell(new PdfPCell(new Phrase("Videoke", columnFont)) { HorizontalAlignment = 1, PaddingBottom = 5f, BackgroundColor = BaseColor.LIGHT_GRAY });
+            tableForCalendarRental.AddCell(new PdfPCell(new Phrase("Customer", columnFont)) { HorizontalAlignment = 1, PaddingBottom = 5f, BackgroundColor = BaseColor.LIGHT_GRAY });
+
+            foreach (var calendarRental in calendarRentals)
+            {
+                tableForCalendarRental.AddCell(new PdfPCell(new Phrase(calendarRental.RentedDate.ToString(), cellFont)) { PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                tableForCalendarRental.AddCell(new PdfPCell(new Phrase(calendarRental.Product, cellFont)) { PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                tableForCalendarRental.AddCell(new PdfPCell(new Phrase(calendarRental.CustomersRent, cellFont)) { PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+            }
+            document.Add(tableForCalendarRental);
+
+            document.Add(Chunk.NEWLINE);
+
+            var NumberOfRentees = calendarRentals.Count();
+
+            PdfPTable tableForCalendarRentalFooter = new PdfPTable(3);
+            float[] widthscellsheaderForCalendarRentalFooter = new float[] { 50f, 100f, 70f };
+            tableForCalendarRentalFooter.SetWidths(widthscellsheaderForCalendarRentalFooter);
+            tableForCalendarRentalFooter.WidthPercentage = 100;
+            tableForCalendarRentalFooter.AddCell(new PdfPCell(new Phrase("", columnFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+            tableForCalendarRentalFooter.AddCell(new PdfPCell(new Phrase("No of Rentees:", columnFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f, HorizontalAlignment = 2 });
+            tableForCalendarRentalFooter.AddCell(new PdfPCell(new Phrase(NumberOfRentees.ToString(), columnFont)) { Border = 0, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+
+            document.Add(tableForCalendarRentalFooter);
 
             // Document End
             document.Close();
